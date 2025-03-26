309222cd2b482c47ef8ed92bb226576d754453ad
Mihail <Mihailzharikow@yandex.ru>
2025-03-26 23:32:33 +0700

    feat(global): rewrite macros for converting void* to type

--- include/global.h
+++ include/global.h
@@ -2,30 +2,9 @@
 #define GLOBAL_H
 
 /**
- * @defgroup macros Macros for converting void* to type
- * @{
+ * @define TO_TYPE(expression,type)
+ * @brief Macros for converting void* to given type
  */
-
-#define INT(expression) *(int *)expression
-#define UINT(expression) *(unsigned int *)expression
-
-#define SHRT(expression) *(short *)expression
-#define USHRT(expression) *(unsigned short *)expression
-
-#define LONG(expression) *(long *)expression
-#define ULONG(expression) *(unsigned long *)expression
-
-#define LLONG(expression) *(long long *)expression
-#define ULLONG(expression) *(unsigned long long *)expression
-
-#define FLOAT(expression) *(float *)expression
-#define DOUBLE(expression) *(double *)expression
-#define LDOUBLE(expression) *(long double *)expression
-
-#define CHAR(expression) *(char *)expression
-#define SCHAR(expression) *(signed char *)expression
-#define UCHAR(expression) *(unsigned char *)expression
-
-/// @}
+#define TO_TYPE(expression, type) *(type *)expression
 
 #endif // GLOBAL_H
