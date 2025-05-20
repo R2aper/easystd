61812bc85f20db4514dc2e12b517d5e7e2d091df
Mihail <Mihailzharikow@yandex.ru>
2025-05-20 22:49:05 +0700

    feat(math): add emath files for functions and type relative to math
    - Add struct and function for Complex numbers

---
+++ include/emath.h
@@ -0,0 +1,40 @@
+#ifndef EMATH_H
+#define EMATH_H
+
+#include "grow.h"
+
+// NOTE: Idk why it's so confusing include M_PI and ets, so i just add this:
+#define _E 2.7182818284590452354         /* e */
+#define _LOG2E 1.4426950408889634074     /* log_2 e */
+#define _LOG10E 0.43429448190325182765   /* log_10 e */
+#define _LN2 0.69314718055994530942      /* log_e 2 */
+#define _LN10 2.30258509299404568402     /* log_e 10 */
+#define _PI 3.14159265358979323846       /* pi */
+#define _PI_2 1.57079632679489661923     /* pi/2 */
+#define _PI_4 0.78539816339744830962     /* pi/4 */
+#define _1_PI 0.31830988618379067154     /* 1/pi */
+#define _2_PI 0.63661977236758134308     /* 2/pi */
+#define _2_SQRTPI 1.12837916709551257390 /* 2/sqrt(pi) */
+#define _SQRT2 1.41421356237309504880    /* sqrt(2) */
+#define _SQRT1_2 0.70710678118654752440  /* 1/sqrt(2) */
+
+/// @brief struct for complex numbers
+typedef struct complex {
+  double Re;
+  double Im;
+} complex;
+
+/// @defgroup Complex Operators on complex numbers
+/// @{
+complex complex_plus(complex c1, complex c2);
+complex complex_minus(complex c1, complex c2);
+complex complex_times(complex c1, complex c2);
+complex complex_divide(complex c1, complex c2);
+complex complex_pow(complex c, long long n);
+///@note grow and it's elements, should be freed after using
+///@return Allocated grow object or NULL
+grow *complex_root(complex c, double n);
+
+///@}
+
+#endif // EMATH_H
---
+++ src/emath.c
@@ -0,0 +1,72 @@
+#include "emath.h"
+#include "grow.h"
+
+#include <math.h>
+
+complex complex_plus(complex c1, complex c2) { return (complex){c1.Re + c2.Re, c1.Im + c2.Im}; }
+complex complex_minus(complex c1, complex c2) { return (complex){c1.Re - c2.Re, c1.Im - c2.Im}; }
+complex complex_times(complex c1, complex c2) {
+  return (complex){c1.Re * c2.Re - c1.Im * c2.Im, c1.Re * c2.Im + c1.Im * c2.Re};
+}
+complex complex_divide(complex c1, complex c2) {
+  double Re = (double)(c1.Re * c2.Re + c1.Im * c2.Im) / (powf(c2.Re, 2 + powf(c2.Im, 2)));
+  double Im = (double)(c1.Im * c2.Re - c1.Re * c2.Im) / (powf(c2.Re, 2 + powf(c2.Im, 2)));
+
+  return (complex){Re, Im};
+}
+
+double complex_module(complex c) { return sqrt(pow(c.Re, 2) + pow(c.Im, 2)); }
+
+double complex_arg(complex c) {
+  if (c.Re > 0)
+    return atan((double)c.Im / c.Re);
+  if (c.Re < 0 && c.Im >= 0)
+    return (_PI + atan((double)c.Im / c.Re));
+  if (c.Re < 0 && c.Im < 0)
+    return (-_PI + atan((double)c.Im / c.Re));
+
+  if (c.Re == 0 && c.Im > 0)
+    return _PI_2;
+  if (c.Re == 0 && c.Im < 0)
+    return -_PI_2;
+
+  return 0;
+}
+
+complex complex_pow(complex c, long long n) {
+  double r = complex_module(c);
+  complex z;
+  z.Re = pow(r, n) * cos(n * complex_arg(c));
+  z.Im = pow(r, n) * sin(n * complex_arg(c));
+
+  return z;
+}
+
+complex complex_first_root(complex c, double n) {
+  complex z;
+  double module = pow(complex_module(c), (double)1 / n);
+  double frac = (double)(complex_arg(c)) / (n);
+  z.Re = module * cos(frac);
+  z.Im = module * sin(frac);
+
+  return z;
+}
+
+grow *complex_root(complex c, double n) {
+  grow *sol = grow_init_empty;
+  double module = pow(complex_module(c), (double)1 / n);
+
+  for (size_t k = 0; k < n; k++) {
+    complex *root = (complex *)malloc(sizeof(complex));
+    if (!root)
+      return NULL;
+    double frac = (double)(complex_arg(c) + 2 * _PI * k) / (n);
+    root->Re = module * cos(frac);
+    root->Im = module * sin(frac);
+
+    if (grow_push(sol, root) != 0)
+      return NULL;
+  }
+
+  return sol;
+}
